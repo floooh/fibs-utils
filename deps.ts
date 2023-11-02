381c69da57cff851486266ed9aa27ac0998f6f18
Andre Weissflog <floooh@gmail.com>
2023-11-02 17:08:01 +0100

    bump fibs to v1.4.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as fibs from 'https://deno.land/x/fibs@v1.3.0/mod.ts';
+export * as fibs from 'https://deno.land/x/fibs@v1.4.0/mod.ts';
 export * as path from 'https://deno.land/std@0.204.0/path/mod.ts';
 export * as fs from 'https://deno.land/std@0.204.0/fs/mod.ts';
