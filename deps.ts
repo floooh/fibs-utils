a4cc344089cdf098830d522edc0f8e83c8b60325
Andre Weissflog <floooh@gmail.com>
2025-12-22 18:54:16 +0100

    fix fibs import

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as fibs from '../fibs/index.ts'; // https://raw.githubusercontent.com/floooh/fibs/master/index.ts';
+export * as fibs from 'https://raw.githubusercontent.com/floooh/fibs/master/index.ts';
 export * as fs from 'jsr:@std/fs';
 export * as path from 'jsr:@std/path';
