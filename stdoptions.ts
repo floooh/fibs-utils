eacc95a8e4373a2c37d796eb7664c01765dac13e
Andre Weissflog <floooh@gmail.com>
2025-12-30 14:18:07 +0100

    stdoptions.ts: add cxx exception/rtti options

--- stdoptions.ts
+++ stdoptions.ts
@@ -3,9 +3,12 @@ import { Builder } from 'jsr:@floooh/fibs';
 
 export function build(b: Builder) {
     if (b.isMsvc()) {
-        b.addCompileOptions(['/W4']);
+        b.addCompileOptions(['/W4', '/EHsc']);
         b.addCompileDefinitions({ _CRT_SECURE_NO_WARNINGS: '1' });
     } else {
         b.addCompileOptions(['-Wall', '-Wextra']);
+        const common_cxx_opts = ['-fno-exceptions', '-fno-rtti'];
+        b.addCompileOptions({ language: 'cxx', opts: common_cxx_opts });
+        b.addLinkOptions({ language: 'cxx', opts: common_cxx_opts });
     }
 }
